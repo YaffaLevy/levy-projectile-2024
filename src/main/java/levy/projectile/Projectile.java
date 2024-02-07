2234bd1b99384c39574db3dfb731029bc4d63190
Yaffa Levy <yaffalevy@Yaffas-Laptop.local>
2024-02-07 18:05:12 -0500

    added PeakY

---
+++ src/main/java/levy/projectile/Projectile.java
@@ -0,0 +1,40 @@
+package levy.projectile;
+
+import static java.lang.Math.sin;
+
+public class Projectile {
+    public double angle;
+    public double radians;
+    public double velocity;
+    public double seconds;
+    public double apexTime;
+    public double peakY;
+
+    public Projectile(double angle, double velocity) {
+        this.angle = angle;
+        this.velocity = velocity;
+        this.radians = Math.toRadians(angle);
+    }
+
+    public void setSeconds(double seconds) {
+        this.seconds = seconds;
+    }
+
+    public double getX() {
+        return Math.cos(radians) * velocity * seconds;
+    }
+
+    public double getY() {
+        return sin(radians) * velocity * seconds - 0.5 * 9.8
+                * seconds * seconds;
+    }
+
+    public double getApexTime() {
+        return velocity * sin(radians) / 9.8;
+    }
+    public double getPeakY() {
+        return ((sin(radians) * velocity) * (sin(radians) * velocity)) / 19.6;
+    }
+
+}
+
